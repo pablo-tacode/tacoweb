b8483e2f22a8853f088c09abe18f6693cd710ddd
karen-tacode <karen@tacode.mx>
2019-09-27 16:49:16 -0500

    fondo color 1: solving margin issue

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ const portrait = window.matchMedia("(orientation: portrait)");
 function changeBackground () {
     backgroundContainer.innerHTML='';
     if(mobile.matches && portrait.matches){
-        backgroundContainer.insertAdjacentHTML('beforeend', `<img src="./src/assets/background-mobile.svg" alt="background-mobile">`)
+        backgroundContainer.insertAdjacentHTML('beforeend', `<object data="./src/assets/background-mobile.svg" alt="background-mobile"></object>`)
     }else if(tablet.matches && portrait.matches){
-        backgroundContainer.insertAdjacentHTML('beforeend', `<img src="./src/assets/background-tablet.svg" alt="background-tablet">`)
+        backgroundContainer.insertAdjacentHTML('beforeend', `<object data="./src/assets/background-tablet.svg" alt="background-tablet"></object>`)
     }else if(desktop.matches && portrait.matches){
-        backgroundContainer.insertAdjacentHTML('beforeend', `<img src="./src/assets/background-desktop.svg" alt="background-desktop">`)
-    }else if(tv.matches && portrait.matches){
-        backgroundContainer.insertAdjacentHTML('beforeend', `<img src="./src/assets/background-TV.svg" alt="background-TV">`)
+        backgroundContainer.insertAdjacentHTML('beforeend', `<object data="./src/assets/background-desktop.svg" alt="background-desktop"></object>`)
+    }else if(tv.matches){
+        backgroundContainer.insertAdjacentHTML('beforeend', `<object data="./src/assets/background-TV.svg" alt="background-TV"></object>`)
     }else {
-        backgroundContainer.insertAdjacentHTML('beforeend', `<img src="./src/assets/background-landscape.svg" alt="background-landscape">`)
+        backgroundContainer.insertAdjacentHTML('beforeend', `<object data="./src/assets/background-landscape.svg" alt="background-landscape"></object>`)
     }
 }
 
