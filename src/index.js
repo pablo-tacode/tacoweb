2ef5524540ccedc37448965a1ec1384191ebaedf
karen-tacode <karen@tacode.mx>
2019-10-01 16:45:04 -0500

    backgrounds 3 and 4

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@
 
 
 bttnChange.addEventListener("click", ()=>{
-    if(backgroundImg.classList.contains("dark")){
-        backgroundImg.classList.remove("dark")
-        backgroundImg.classList.add("light")
+    if(backgroundImg.classList.contains("dark1")){
+        backgroundImg.classList.remove("dark1")
+        backgroundImg.classList.add("dark2")
+    }else if(backgroundImg.classList.contains("dark2")){
+        backgroundImg.classList.remove("dark2")
+        backgroundImg.classList.add("dark3")
+    }else if(backgroundImg.classList.contains("dark3")){
+        backgroundImg.classList.remove("dark3")
+        backgroundImg.classList.add("dark4")
     }else{
-        backgroundImg.classList.remove("light")
-        backgroundImg.classList.add("dark")
+        backgroundImg.classList.remove("dark4")
+        backgroundImg.classList.add("dark1")
     }
 })
 
