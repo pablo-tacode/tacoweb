9b98709c22b0565546f8e020b9f8f0b6b478d892
olga-tacode <olga@tacode.mx>
2019-10-08 09:02:27 -0500

    adding commit to merge

---
+++ language.js
@@ -0,0 +1,25 @@
+const language =()=>{
+    $("[data-localize]").localize("mylanguage", { language: "es" });
+   
+      
+}
+document.getElementById("es").addEventListener("click", language);
+
+const original=()=>{
+    $("[data-localize]").localize("mylanguage", { language: "en" });
+  
+}
+document.getElementById("en").addEventListener("click", original);
+
+
+const languageOne=()=>{
+    $("[data-localize]").localize("mylanguage", { language: "es" });
+   
+}
+document.getElementById("es-mobile").addEventListener("click", languageOne);
+
+const originalOne=()=>{
+    $("[data-localize]").localize("mylanguage", { language: "en" });
+  
+}
+document.getElementById("en-mobile").addEventListener("click", originalOne);
