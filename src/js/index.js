e0b7e0deb525c5bc3e083f06f3aa584c6250b8f1
karen-tacode <karen@tacode.mx>
2019-12-03 18:03:24 -0600

    Carousel with animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 /* General elements */
 const sections = document.querySelectorAll("article");
+const targetWidth = 1024;
 
 /* Background elements */
 const mountainOne = document.getElementById("mountain-001");
 const mountainTwo = document.getElementById("mountain-002");
 const cities = document.getElementById("cities-001");
+let xPercent, mountainScale;
 
 /* Menu elements */
 const menuLinks = document.querySelectorAll(".header-links");
@@ -33,8 +35,10 @@ const tagline = document.getElementById("tagline");
 const servicesIcon = document.getElementById("service");
 const servicesLink = document.getElementById("services-link");
 const slides = document.querySelectorAll(".carousel-img");
-const prevArrow = document.getElementById("prev");
+/* const prevArrow = document.getElementById("prev"); */
 const nextArrow = document.getElementById("next");
+let slideIndex = 1;
+
 
 
 /* General Functions */
@@ -42,12 +46,12 @@ function changePage(link) {
   zeppelinFloating.pause();
   sections.forEach(section => {
     if (link.classList.contains(`${section.id}`)) {
-      fadeIn(section);
       if (section.id === "homes") {
         zeppelinFloating.play();
-        homeBackgroundEntrance()
+        homeBackgroundEntrance();
         section.classList.add("active");
       } else {
+        fadeIn(section);
         section.classList.add("active");
         homeBackgroundExit();
       }
@@ -56,17 +60,13 @@ function changePage(link) {
 }
 
 function focusAnchor(link) {
-  link.classList.remove("innactive")
+  link.classList.remove("innactive");
   link.classList.add("active");
 }
 
 function removeClass(element) {
   element.classList.remove("active");
-  element.classList.add("innactive")
-}
-
-function sideIconsOnFocus(link) {
-  link.firstChild.style.opacity = "1";
+  element.classList.add("innactive");
 }
 
 function changeBackgroundColor(link) {
@@ -85,52 +85,15 @@ function removeActiveClass(arr) {
   });
 }
 
-const hideEn = () => {
-  en.classList.add("hide");
-  es.classList.remove("hide");
+const hideEn = (english, spanish) => {
+  english.classList.add("hide");
+  spanish.classList.remove("hide");
 };
 
-const hideEs = () => {
-  es.classList.add("hide");
-  en.classList.remove("hide");
+const hideEs = (spanish, english) => {
+  spanish.classList.add("hide");
+  english.classList.remove("hide");
 };
-
-const hideEng = () => {
-  enMobile.classList.add("hide");
-  esMobile.classList.remove("hide");
-};
-
-const hideSpa = () => {
-  esMobile.classList.add("hide");
-  enMobile.classList.remove("hide");
-};
-
-
-let xPercent, mountainScale
-
-function listenToScreenWidth(w) {
-  const targetWidth = 1024;
-  if (w <= targetWidth) {
-    xPercent = "0%"
-    mountainScale = 1
-    showSlides(slideIndex);
-    if(home.classList.contains('active')){
-      console.log('Home is active')
-      }else{
-        mountainOne.style.transform = "matrix(1, 0, 0, 1, 0, 0)";
-      }
-  } else {
-    xPercent = "23%"
-    mountainScale = 3
-    slides.forEach(slide => slide.removeAttribute('style'));
-    if(home.classList.contains('active')){
-      console.log('Home is active')
-      }else{
-        mountainOne.style.transform = "translate(23%, 0%) matrix(3, 0, 0, 1, 0, 0)";
-      }
-}}
-
-let slideIndex = 1;
 
 function showSlides(n) {
   if (n > slides.length) {
@@ -140,25 +103,79 @@ function showSlides(n) {
     slideIndex = slides.length
   }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    bye(slides[i])
   }
   slides[slideIndex - 1].style.display = "flex";
+  entranceFromRight(slides[slideIndex - 1]);
 }
 
-
 function addingSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideIndex += n)
+}
+
+function isHomeActiveMobile() {
+  if (home.classList.contains('active')) {
+    console.log('Home is active');
+  } else {
+    mountainOne.style.transform = "matrix(1, 0, 0, 1, 0, 0)";
+  }
+}
+
+function isHomeActiveDesktop() {
+  slides.forEach(slide => slide.removeAttribute('style'));
+  if (home.classList.contains('active')) {
+    console.log('Home is active');
+  } else {
+    mountainOne.style.transform = "translate(23%, 0%) matrix(3, 0, 0, 1, 0, 0)";
+  }
+}
+
+function listenToScreenWidth(w) {
+  console.log('listen to screen width')
+  if (w <= targetWidth) {
+    xPercent = 0
+    mountainScale = 1
+    showSlides(slideIndex);
+    isHomeActiveMobile();
+  } else {
+    xPercent = 23
+    mountainScale = 3
+    isHomeActiveDesktop();
+  }
 }
 
 /* ------------------------------------- GSAP Animation functions ------------------------------------- */
 
 function fadeIn(section) {
-  TweenMax.fromTo(section, 1.5, {
-    x: "-100%"
+  const tl = new TimelineMax();
+  tl.fromTo(section, 1.5, {
+      xPercent: -100
+    }, {
+      xPercent: 0,
+      zIndex: 1
+    })
+    .fromTo(".arrows", 1, {
+      opacity: 0
+    }, {
+      opacity: 1
+    }, 1.3);
+}
+
+function entranceFromRight(elementOne) {
+  TweenMax.fromTo(elementOne, 1, {
+    xPercent: -200,
+    width: 0
   }, {
-    x: "0%",
-    zIndex: 1
-  });
+    xPercent: 0,
+    width: "80%"
+  })
+}
+
+function bye(element) {
+  TweenMax.to(element, 1, {
+      xPercent: 200,
+      width: 0
+    })
 }
 
 const zeppelinFloating = TweenMax.to(zeppelin, 2, {
@@ -180,10 +197,10 @@ function homeBackgroundExit() {
       onComplete: focusAnchor(mountainOne)
     }, 0.1)
     .to(mountainTwo, 1, {
-      y: "100%"
+      onStart: focusAnchor(mountainTwo)
     }, 0.1)
     .to(cities, 1, {
-      y: "100%"
+      yPercent: 100
     }, 0.1)
     .to(tagline, 0.5, {
       opacity: 0
@@ -198,13 +215,19 @@ function homeBackgroundEntrance() {
     }, 0.1)
     .to(mountainOne, 1, {
       onStart: removeClass(mountainOne),
-      xPercent: "0%"
+      xPercent: 0
     }, 0.1)
     .to(mountainTwo, 1, {
-      y: "0%"
+      onStart: removeClass(mountainTwo)
     }, 0.1)
     .to(cities, 1, {
-      y: "0%"
+      yPercent: 0
+    }, 0.1)
+    .fromTo(home, 1.5, {
+      xPercent: -100
+    }, {
+      xPercent: 0,
+      zIndex: 1
     }, 0.1)
     .to(tagline, 1, {
       opacity: 1
@@ -252,22 +275,34 @@ zeppelin.addEventListener("click", function (e) {
   homeBackgroundExit();
 });
 
-en.addEventListener("click", hideEn);
-
-es.addEventListener("click", hideEs);
-
-enMobile.addEventListener("click", hideEng);
-
-esMobile.addEventListener("click", hideSpa);
-
-prevArrow.addEventListener("click", (e) => {
-  e.preventDefault()
-  addingSlides(-1)
+en.addEventListener("click", (e) => {
+  e.preventDefault();
+  hideEn(en, es);
 });
 
+es.addEventListener("click", (e) => {
+  e.preventDefault();
+  hideEs(es, en);
+});
+
+enMobile.addEventListener("click", (e) => {
+  e.preventDefault();
+  hideEn(enMobile, esMobile);
+});
+
+esMobile.addEventListener("click", (e) => {
+  e.preventDefault();
+  hideEs(esMobile, enMobile);
+});
+
+/* prevArrow.addEventListener("click", (e) => {
+  e.preventDefault();
+  addingSlides(-1);
+}); */
+
 nextArrow.addEventListener("click", (e) => {
-  e.preventDefault()
-  addingSlides(1)
+  e.preventDefault();
+  addingSlides(1);
 });
 
 /* ------------------------------------- CSSOM EVENTS ------------------------------------- */
