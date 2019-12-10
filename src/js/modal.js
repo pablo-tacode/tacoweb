b75aa7c0fbcbb534581b974b42b43963d08661f2
karen-tacode <karen@tacode.mx>
2019-12-10 15:20:38 -0600

    Modal layout. Missing: match styles to UI design

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,12 @@
 const openModal = document.getElementById('open-modal-one');
 const openModalTwo = document.getElementById('open-modal-two')
-const closeModal = document.querySelector('.close-modal');
+const closeModal = document.getElementById('close-modal-one');
+const closeModalTwo = document.getElementById('close-modal-two');
 const modal = document.querySelector('.modal');
 
 const modalPuerquiAhorro = `
-            <div id="modal-one">
-                <a class="close-modal"><img src="./src/assets/images/close_bttn.svg" alt="Boton para cerrar modal"></a>
+            <div class="modal-content" id="modal-one">
+                <a class="close-modal" id="close-modal-one"><img src="./src/assets/images/close_bttn.svg" alt="Boton para cerrar modal"></a>
                 <div class="modal-text">
                     <h3>PUERQUI AHORRO</h3>
                     <ul>
@@ -17,15 +18,15 @@ const modalPuerquiAhorro = `
                         <li>Simulador de caja de ahorro</li>
                     </ul>
                 </div>
-                <img src="./src/assets/images/PuerquiAhorro_001.png" alt="Primera pantalla de la aplicacion puerqui ahorro">
-                <img src="./src/assets/images/PuerquiAhorro_002.png" alt="Segunda pantalla de la aplicacion puerqui ahorro">
-                <img src="./src/assets/images/PuerquiAhorro_003.png" alt="Tercera pantalla de la aplicacion puerqui ahorro">
+                <img class="modal-img-one" src="./src/assets/images/PuerquiAhorro_001.png" alt="Primera pantalla de la aplicacion puerqui ahorro">
+                <img class="modal-img-two" src="./src/assets/images/PuerquiAhorro_002.png" alt="Segunda pantalla de la aplicacion puerqui ahorro">
+                <img class="modal-img-three" src="./src/assets/images/PuerquiAhorro_003.png" alt="Tercera pantalla de la aplicacion puerqui ahorro">
             </div>
 `
 
 const modalMedicalBoard = `
-            <div id="modal-two">
-                <a class="close-modal"><img src="./src/assets/images/close_bttn.svg" alt="Boton para cerrar modal"></a>
+            <div class="modal-content" id="modal-two">
+                <a class="close-modal" id="close-modal-two"><img src="./src/assets/images/close_bttn.svg" alt="Boton para cerrar modal"></a>
                 <div class="modal-text">
                     <h3>MEDICAL BOARD</h3>
                     <ul>
@@ -37,32 +38,40 @@ const modalMedicalBoard = `
                         <li>Seguimiento de tratamientos</li>
                     </ul>
                 </div>
-                <img src="./src/assets/images/medicalBoard_001.png" alt="Primera pantalla de la aplicacion medical board">
-                <img src="./src/assets/images/medicalBoard_002.png" alt="Segunda pantalla de la aplicacion medical board">
-                <img src="./src/assets/images/medicalBoard_003.png" alt="Tercera pantalla de la aplicacion medical board">
+                <img modal-img-one src="./src/assets/images/medicalBoard_001.png" alt="Primera pantalla de la aplicacion medical board">
+                <img modal-img-two src="./src/assets/images/medicalBoard_002.png" alt="Segunda pantalla de la aplicacion medical board">
+                <img modal-img-three src="./src/assets/images/medicalBoard_003.png" alt="Tercera pantalla de la aplicacion medical board">
             </div>
 `
 
-function openingModal () {
+function openingModal() {
     modal.style.display = 'grid';
 }
 
-function closingModal () {
+function closingModal() {
     modal.style.display = 'none';
 }
 
 openModal.addEventListener('click', (e) => {
     e.preventDefault();
     openingModal();
-    modal.innerHTML='';
+    modal.innerHTML = '';
     modal.insertAdjacentHTML('beforeend', modalPuerquiAhorro)
 });
 
 openModalTwo.addEventListener('click', (e) => {
     e.preventDefault();
     openingModal();
-    modal.innerHTML='';
+    modal.innerHTML = '';
     modal.insertAdjacentHTML('beforeend', modalMedicalBoard)
 });
 
-closeModal.addEventListener('click', closingModal);
+closeModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    closingModal();
+});
+
+closeModalTwo.addEventListener('click', (e) => {
+    e.preventDefault();
+    closingModal();
+});
