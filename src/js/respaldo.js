9319b9fe07099739da1c3acb308a6c81800ae184
karen-tacode <karen@tacode.mx>
2019-12-11 13:19:37 -0600

    carousel finished

--- src/js/respaldo.js
+++ src/js/respaldo.js
@@ -441,3 +441,24 @@ window.addEventListener(
 if (/iPhone/i.test(navigator.userAgent)) {
   document.querySelector("html").classList.add("iphone");
 }
+
+
+let slideIndex = [1,1];
+let slideId = ["slides-services", "slides-projects"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.querySelectorAll(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
